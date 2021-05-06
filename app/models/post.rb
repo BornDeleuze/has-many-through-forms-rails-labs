68721ecce2319cd75de8b1ab27d83a7bf5268c1d
Matt Beaudoin <ehxtwo@gmail.com>
2021-05-06 16:00:22 -0700

    commit tree 31a50e865c23aa1585f21f486b39aeea443b5f8e

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
-
+  def categories_attributes=(categories_hashes)
+    categories_hashes.each do |i, category_attributes|
+      cat = Category.find_or_create_by(name: category_attributes[:name])
+      self.post_categories.build(:category => cat)
+    end
+  end
 end
